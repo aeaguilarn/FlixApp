6a8a12dd460383fbfc84f9330739bd2bedc1b2dc
aeaguilarn <aeaguilarn@gmail.com>
2020-06-11 12:30:48 -0400

    First commit

---
+++ FlixApp/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  FlixApp
+//
+//  Created by Andrés Aguilar on 6/11/20.
+//  Copyright © 2020 Andrés Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
